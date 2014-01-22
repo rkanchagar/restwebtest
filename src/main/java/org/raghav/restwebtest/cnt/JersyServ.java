65b229dab5703df3dbc1eff9cf8228ca95ff4359
rk <rkanchagar@gmail.com>
2014-01-22 08:29:13 +0530

    Added Lib in webinf
    Added lib files in web-inf

--- src/main/java/org/raghav/restwebtest/cnt/JersyServ.java
+++ src/main/java/org/raghav/restwebtest/cnt/JersyServ.java
@@ -6,18 +6,37 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 
-@Path("/jrsy")
+@Path("/jrsy/")
 public class JersyServ {
 	
 	@GET
-	@Produces(MediaType.TEXT_HTML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public String returnout()
-	{
-		
-		return "Jersy class call";
-		
-		
+	{	
+		return "Jersy : class call";
 	}
+	
+	@GET
+	  @Produces(MediaType.TEXT_PLAIN)
+	  public String sayPlainTextHello() {
+	    return "Hello Jersey Plain";
+	  }
+
+	  // This method is called if XML is request
+	  @GET
+	  @Produces(MediaType.TEXT_XML)
+	  public String sayXMLHello() {
+	    return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey XML" + "</hello>";
+	  }
+
+	  // This method is called if HTML is request
+	  @GET
+	  @Produces(MediaType.TEXT_HTML)
+	  public String sayHtmlHello() {
+	    return "<html> " + "<title>" + "Hello Jersey HTML" + "</title>"
+	        + "<body><h1>" + "Hello Jersey HTML" + "</body></h1>" + "</html> ";
+	  }
+
 	
 	
 
